cdd4529f358df44f8ebf6a3a7aecfc81e2f52d1b
Camila da Costa <camila.coosta99@gmail.com>
2023-04-12 21:28:03 -0300

    editar/excluir usuario e validações

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -11,4 +11,10 @@
     <li>{{ $user->email }}</li>
 </ul>
 
+<form action="{{ route('users.delete', $user->id) }}" method="post">
+    @method('DELETE')
+    @csrf
+    <button>Deletar</button>
+</form>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::delete('/users/{id}', [UserController::class, 'delete'])->name('users.delete');
+
+Route::put('users/{id}', [UserController::class, 'update'])->name('users.update');
+
+Route::get('users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
 Route::get('users/create', [UserController::class, 'create'])->name('users.create');
